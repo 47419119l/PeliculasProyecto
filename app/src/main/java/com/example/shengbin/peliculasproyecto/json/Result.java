e4e9ea9aa404e600598fbfa14043dbf40c8d2653
sandra <47419119l@iespoblenou.org>
2015-11-08 11:53:53 +0100

    Objectes JSON serialitzables

--- app/src/main/java/com/example/shengbin/peliculasproyecto/json/Result.java
+++ app/src/main/java/com/example/shengbin/peliculasproyecto/json/Result.java
@@ -1,5 +1,6 @@
 package com.example.shengbin.peliculasproyecto.json;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -7,7 +8,7 @@ import java.util.Map;
 import javax.annotation.Generated;
 
 @Generated("org.jsonschema2pojo")
-public class Result {
+public class Result implements Serializable {
 
     private boolean adult;
     private String backdropPath;
